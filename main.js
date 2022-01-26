36139e3b7f70db2275f3ba4d47f41de0a6a38fc3
Thomas Heaton <thomas.m.heaton@gmail.com>
2022-01-26 20:56:02 +0000

    cleanup :broom:

--- main.js
+++ main.js
@@ -1,260 +1,16 @@
-
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
-22
-23
-24
-25
-26
-27
-28
-29
-30
-31
-32
-33
-34
-35
-36
-37
-38
-39
-40
-41
-42
-43
-44
-45
-46
-47
-48
-49
-50
-51
-52
-53
-54
-55
-56
-57
-58
-59
-60
-61
-62
-63
-64
-65
-66
-67
-68
-69
-70
-71
-72
-73
-74
-75
-76
-77
-78
-79
-80
-81
-82
-83
-84
-85
-86
-87
-88
-89
-90
-91
-92
-93
-94
-95
-96
-97
-98
-99
-100
-101
-102
-103
-104
-105
-106
-107
-108
-109
-110
-111
-112
-113
-114
-115
-116
-117
-118
-119
-120
-121
-122
-123
-124
-125
-126
-127
-128
-129
-130
-131
-132
-133
-134
-135
-136
-137
-138
-139
-140
-141
-142
-143
-144
-145
-146
-147
-148
-149
-150
-151
-152
-153
-154
-155
-156
-157
-158
-159
-160
-161
-162
-163
-164
-165
-166
-167
-168
-169
-170
-171
-172
-173
-174
-175
-176
-177
-178
-179
-180
-181
-182
-183
-184
-185
-186
-187
-188
-189
-190
-191
-192
-193
-194
-195
-196
-197
-198
-199
-200
-201
-202
-203
-204
-205
-206
-207
-208
-209
-210
-211
-212
-213
-214
-215
-216
-217
-218
-219
-220
-221
-222
-223
-224
-225
-226
-227
-228
-229
-230
-231
-232
-233
-234
-235
-236
-237
-238
 main();
 
-//
-// Start here
-//
 function main() {
     const canvas = document.querySelector('#glcanvas');
     const gl = canvas.getContext('webgl');
 
     // If we don't have a GL context, give up now
-
     if (!gl) {
         alert('Unable to initialize WebGL. Your browser or machine may not support it.');
         return;
     }
 
     // Vertex shader program
-
     const vsSource = `
     attribute vec4 aVertexPosition;
  
@@ -262,25 +18,22 @@ function main() {
     uniform mat4 uProjectionMatrix;
  
     void main() {
-      gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+        gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
     }
-  `;
+    `;
 
     // Fragment shader program
-
     const fsSource = `
     void main() {
-      gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+        gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
     }
-  `;
+    `;
 
-    // Initialize a shader program; this is where all the lighting
-    // for the vertices and so forth is established.
+    // Initialize a shader program; this is where all the lighting for the vertices and so forth is established.
     const shaderProgram = initShaderProgram(gl, vsSource, fsSource);
 
     // Collect all the info needed to use the shader program.
-    // Look up which attribute our shader program is using
-    // for aVertexPosition and look up uniform locations.
+    // Look up which attribute our shader program is using for aVertexPosition and look up uniform locations.
     const programInfo = {
         program: shaderProgram,
         attribLocations: {
@@ -292,33 +45,23 @@ function main() {
         },
     };
 
-    // Here's where we call the routine that builds all the
-    // objects we'll be drawing.
+    // Here's where we call the routine that builds all the objects we'll be drawing.
     const buffers = initBuffers(gl);
 
     // Draw the scene
     drawScene(gl, programInfo, buffers);
 }
 
-//
-// initBuffers
-//
-// Initialize the buffers we'll need. For this demo, we just
-// have one object -- a simple two-dimensional square.
-//
+// Initialize the buffers we'll need. For this demo, we just have one object -- a simple two-dimensional square.
 function initBuffers(gl) {
 
     // Create a buffer for the square's positions.
-
     const positionBuffer = gl.createBuffer();
 
-    // Select the positionBuffer as the one to apply buffer
-    // operations to from here out.
-
+    // Select the positionBuffer as the one to apply buffer operations to from here out.
     gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
     // Now create an array of positions for the square.
-
     const positions = [
         1.0,  1.0,
         -1.0,  1.0,
@@ -326,10 +69,8 @@ function initBuffers(gl) {
         -1.0, -1.0,
     ];
 
-    // Now pass the list of positions into WebGL to build the
-    // shape. We do this by creating a Float32Array from the
-    // JavaScript array, then use it to fill the current buffer.
-
+    // Now pass the list of positions into WebGL to build the shape.
+    // We do this by creating a Float32Array from the JavaScript array, then use it to fill the current buffer.
     gl.bufferData(gl.ARRAY_BUFFER,
         new Float32Array(positions),
         gl.STATIC_DRAW);
@@ -344,12 +85,11 @@ function initBuffers(gl) {
 //
 function drawScene(gl, programInfo, buffers) {
     gl.clearColor(0.0, 0.0, 0.0, 1.0);  // Clear to black, fully opaque
-    gl.clearDepth(1.0);                 // Clear everything
-    gl.enable(gl.DEPTH_TEST);           // Enable depth testing
-    gl.depthFunc(gl.LEQUAL);            // Near things obscure far things
+    gl.clearDepth(1.0);                                 // Clear everything
+    gl.enable(gl.DEPTH_TEST);                                 // Enable depth testing
+    gl.depthFunc(gl.LEQUAL);                                  // Near things obscure far things
 
     // Clear the canvas before we start drawing on it.
-
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
     // Create a perspective matrix, a special matrix that is
@@ -359,30 +99,31 @@ function drawScene(gl, programInfo, buffers) {
     // and we only want to see objects between 0.1 units
     // and 100 units away from the camera.
 
-    const fieldOfView = 45 * Math.PI / 180;   // in radians
+    const fieldOfView = 45 * Math.PI / 180;  // in radians
     const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
     const zNear = 0.1;
     const zFar = 100.0;
     const projectionMatrix = mat4.create();
 
-    // note: glmatrix.js always has the first argument
-    // as the destination to receive the result.
-    mat4.perspective(projectionMatrix,
+    // NOTE: glmatrix.js always has the first argument as the destination to receive the result.
+    mat4.perspective(
+        projectionMatrix,
         fieldOfView,
         aspect,
         zNear,
-        zFar);
+        zFar
+    );
 
     // Set the drawing position to the "identity" point, which is
     // the center of the scene.
     const modelViewMatrix = mat4.create();
 
-    // Now move the drawing position a bit to where we want to
-    // start drawing the square.
-
-    mat4.translate(modelViewMatrix,     // destination matrix
+    // Now move the drawing position a bit to where we want to start drawing the square.
+    mat4.translate(
+        modelViewMatrix,     // destination matrix
         modelViewMatrix,     // matrix to translate
-        [-0.0, 0.0, -6.0]);  // amount to translate
+        [-0.0, 0.0, -6.0]
+    );  // amount to translate
 
     // Tell WebGL how to pull out the positions from the position buffer into the vertexPosition attribute.
     {
@@ -410,11 +151,13 @@ function drawScene(gl, programInfo, buffers) {
     gl.uniformMatrix4fv(
         programInfo.uniformLocations.projectionMatrix,
         false,
-        projectionMatrix);
+        projectionMatrix
+    );
     gl.uniformMatrix4fv(
         programInfo.uniformLocations.modelViewMatrix,
         false,
-        modelViewMatrix);
+        modelViewMatrix
+    );
 
     {
         const offset = 0;
